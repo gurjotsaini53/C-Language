954e1c86e49b66c643260379d79b9df397c93bbb
gurjotsaini53 <gurjotsaini53@gmail.com>
2022-09-23 11:12:30 +0530

    modifying control instruction file and adding control instruction executable file

--- Instructions_in_c/Control-Instruction.c
+++ Instructions_in_c/Control-Instruction.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 int main(){
+    /*
    control statement or instructions
 
    1. sequence control 
-   2. 
+   2. Decision control
+   3. Loop control
+   4. case Control
+  */
+
+ printf("%d\n",4==4);
+//  1--- means true 
+
+ printf("%d\n",4==40);
+// 2 -- means false
+
+
+ printf("%d\n",4>4);
+ printf("%d\n",4>=4);
+ printf("%d\n",5 >=4);
+ printf("%d\n",5 !=4);
+
+
 return 0 ;
 }
