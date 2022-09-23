62e450001cabe2d043ba58f960b77142cc2b35e8
gurjotsaini53 <gurjotsaini53@gmail.com>
2022-09-23 11:30:26 +0530

    add more problem files

---
+++ Instructions_in_c/problem/even-odd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int num  ;
+    printf("Enter a number  : ");
+    scanf("%d",&num);
+   if (num % 2 ==0)
+    printf("---EVEN---\n");
+
+
+   else
+    printf("---ODD---\n");
+
+
+  return 0; 
+}
---
+++ Instructions_in_c/problem/valid-or-not-valid-statement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    // check validity ;
+
+    // int a = 8^8;
+    //    printf("%d\n",8^8);  ---valid
+
+    // int x ; int y = x ; -- valid
+
+    // int x ,y = x ; -- invalid
+
+    //  char star ='**';  -- invlid
+
+    return 0;
+}
