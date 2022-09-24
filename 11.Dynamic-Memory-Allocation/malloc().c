70c595a14a2f92142513a53730d21e57920f5ff9
gurjotsaini53 <gurjotsaini53@gmail.com>
2022-09-24 16:26:04 +0530

    malloc

---
+++ 11.Dynamic-Memory-Allocation/malloc().c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    /*
+     malloc()--memory allocation
+
+     takes memory of bytes ti be allocated % return a pointer of type void 
+
+     ptr = (*int)malloc(5*sizeof(int));
+
+    */
+
+   return 0 ;
+}
